617c5cefbab7f322ce5a83a08fd83691e5ca7c18
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-19 15:29:31 -0400

    Update 3dgnr.hs

--- 3dgnr.hs
+++ 3dgnr.hs
@@ -1,3 +1,3 @@
 main = do
   putStrLn "These are useful geometric numbers:"
-  putStrLn ("Tetrahedral, Square Pyramidal, Octahedral, Cubic; " ++ show [1, 4, 5, 6, 8, 10, 14, 20, 27, 30, 34, 35, 54, 55, 56, 64])
+  putStrLn ("Tetrahedral, Square Pyramidal, Triangular Bipyramid, Triangular Prism, Octahedral, Cubic; " ++ show [1, 4, 5, 6, 8, 10, 12, 14, 20, 27, 30, 34, 35, 42, 54, 55, 56, 64])
