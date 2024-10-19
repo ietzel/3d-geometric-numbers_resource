2e989560a1d908f1215d2f984ac65dc43a43a6ab
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-19 15:11:27 -0400

    Update and rename 3dgnr.gms to 3dgnr.hs

---
+++ 3dgnr.hs
@@ -0,0 +1,3 @@
+main = do
+  putStrLn "These are useful geometric numbers:"
+  putStrLn ("Tetrahedral, Square Pyramidal, Octahedral, Cubic; " ++ show [1, 4, 5, 6, 8, 10, 14, 20, 27, 30, 34, 35, 54, 55, 56, 64])
